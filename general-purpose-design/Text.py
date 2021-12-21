cabef79bf38be54dc9cd0962512f4392eb91ca06
Nyptop <neil.gibbons@hotmail.com>
2021-12-21 10:56:14 +0000

    working general-purpose-design

--- general-purpose-design/Text.py
+++ general-purpose-design/Text.py
@@ -1,25 +1,18 @@
 class Text:
     def __init__(self):
         self._text = ''
-        self._cursor_position = 0
 
-    def insert(self, position, char):
-        self._text += char
+    def __len__(self):
+        return len(self._text)
 
-    def delete(self, start_position, end_position):
-        # deletes position immediately to the right of the cursor
-        self._text = self._text[:self._cursor_position] + self._text[self._cursor_position+1:]
+    def insert(self, char, position):
+        self._text = self._text[:position] + char + self._text[position:]
 
-    def cursor_left(self):
-        # should I move some of this logic out as well? Cursor logic now in the user interface?
-        if self._cursor_position > 0:
-            self._cursor_position -= 1
-            print(self._cursor_position)
-
-    def cursor_right(self):
-        if self._cursor_position <= len(self._text):
-            self._cursor_position += 1
-        print(self._cursor_position)
+    def delete(self, start, end=None):
+        if end:
+            self._text = self._text[:start] + self._text[end:]
+        else: # deleting from a position until the end
+            self._text = self._text[:start]
 
     def get_text(self):
         return self._text
--- general-purpose-design/UserInterface.py
+++ general-purpose-design/UserInterface.py
@@ -1,5 +1,7 @@
 import pygame
 
+# perhaps best to also create a UI class for both specialised and general purpose
+
 from Text import Text
 
 pygame.init()
@@ -10,6 +12,7 @@ myfont = pygame.font.SysFont('Comic Sans MS', 30)
 screen = pygame.display.set_mode([500, 500])
 
 text = Text()
+cursor_position = 0
 
 running = True
 while running:
@@ -19,21 +22,38 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
-                text.add_char('q')
+                text.insert('q', cursor_position)
+                cursor_position += 1
             if event.key == pygame.K_w:
-                text.add_char('w')
+                text.insert('w', cursor_position)
+                cursor_position += 1
             if event.key == pygame.K_DELETE:
-                text.delete()
+                text.delete(start=-1)
+                cursor_position = min(len(text), cursor_position + 1)
             if event.key == pygame.K_BACKSPACE:
-                text.backspace()
-            if event.key == pygame.K_LEFT:
-                text.cursor_left()
-            if event.key == pygame.K_RIGHT:
-                text.cursor_right()
+                text.delete(start=-1)
+                cursor_position = max(0, cursor_position - 1)
+            if event.key == pygame.K_LEFT and cursor_position > 0:
+                cursor_position -= 1
+            if event.key == pygame.K_RIGHT and cursor_position <= len(text):
+                cursor_position += 1
 
     screen.fill((255, 255, 255))
 
-    for line_number, line in enumerate(text.get_text()):
+    # it makes sense to move this logic here (or into UI class) and make the text class more
+    # general purpose. Does the text class need to have a concept of line
+    # length? No, this is something visual, so belongs to the user 
+    # interface. 
+    lines = [''] 
+    for char_number, char in enumerate(text.get_text()):
+        if char_number == cursor_position:
+            lines[-1] += 'a'
+        else:
+            lines[-1] += char
+        if len(lines[-1]) > 30:
+            lines.append('')
+
+    for line_number, line in enumerate(lines):
         textsurface = myfont.render(line, False, (0, 0, 0))
         screen.blit(textsurface,(0,line_number*20))
 
